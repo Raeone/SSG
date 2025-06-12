6431c3826339c5b277120be5fa6d63f63affecaa
Raeone <pay124@seznam.cz>
2025-06-12 12:35:09 +0200

    test

--- src/block_markdowns.py
+++ src/block_markdowns.py
@@ -1,3 +1,12 @@
+from enum import Enum
+
+class BlockType(Enum):
+  PARAGRAPH = "paragraph"
+  HEADING = "heading"
+  QUOTE = "quote"
+  UNORDERED_LIST = "unordered_list"
+  ORDERED_LIST = "ordered_list"
+
 def markdown_to_blocks(markdown): # string -> list of str
   """Takes raw text in markdown and split it into list of strings, where each string is a block separated with \n\n (empty line)
 
@@ -11,3 +20,6 @@ def markdown_to_blocks(markdown): # string -> list of str
   block_strings = list(map(lambda s: s.strip(), block_strings))
   block_strings = list(filter(lambda s: s, block_strings))  
   return block_strings
+
+def block_to_block_type(): # string -> BlockType(Enum)
+    
