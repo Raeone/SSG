d13886233f2abced3a89ded8da26c2b9fafecc61
Raeone <pay124@seznam.cz>
2025-06-17 17:21:58 +0200

    comments

--- src/document_markdowns.py
+++ src/document_markdowns.py
@@ -9,6 +9,15 @@ from block_markdowns import (
 )
 
 def markdown_to_html_node(markdown): 
+  """Final function to put all together. It takes markdkown text and divides it into block of text. For each block it creates HTMLNode object and for content it generate children HTMLNode objects. 
+  Returns one final <div> tag with all content. 
+
+  Args:
+      markdown (string): markdown document
+
+  Returns:
+      HTMLNode: structure of html nodes (converted markdown document)
+  """
   blocks = markdown_to_blocks(markdown)
   children = []
   for block in blocks:
@@ -17,6 +26,14 @@ def markdown_to_html_node(markdown):
   return ParentNode("div", children, None)
 
 def block_to_html(block):  
+  """Helper function for markdown_to_html_node. It takes block of text, finds out its block_type and returns HTMLNode object structure (with children), based on block type. 
+
+  Args:
+      block (string): block of markdown text
+
+  Returns:
+      HTMLNode: html structure representing md document
+  """
   block_type = block_to_block_type(block)
   if block_type == BlockType.PARAGRAPH:
     return paragraph_to_html(block)  
@@ -32,6 +49,14 @@ def block_to_html(block):
     return olist_to_html(block)
 
 def olist_to_html(block):
+  """Helper for block_to_html fn. Converts ordered list (md) to HTMLNode structure.
+
+  Args:
+      block (string): markdown text
+
+  Returns:
+      HTMLNode
+  """
   items = []
   lines = block.splitlines()
   for line in lines: 
@@ -41,6 +66,14 @@ def olist_to_html(block):
   return ParentNode("ol", items)
 
 def ulist_to_html(block):
+  """Helper. Converts unordered list (md) into HTMLNode structure objects. 
+
+  Args:
+      block (string)
+
+  Returns:
+      HTMLNode
+  """
   items = []
   lines = block.splitlines() 
   for line in lines:
@@ -50,6 +83,17 @@ def ulist_to_html(block):
   return ParentNode("ul", items)
 
 def quote_to_html(block):
+  """Helper. Converts blockquote md text into HTMLNode objects structure.
+
+  Args:
+      block (string): 
+
+  Raises:
+      ValueError: if not starting with ">" not a blockquote
+
+  Returns:
+      HTMLNode: 
+  """
   line_content = [] # ["line content", ]
   lines = block.splitlines() # ['>line', ]
   for line in lines:
@@ -61,6 +105,17 @@ def quote_to_html(block):
   return ParentNode("blockquote", children)
 
 def code_to_html(block):
+  """Helper. Convert code (md) into HTMLNodes
+
+  Args:
+      block (string): 
+
+  Raises:
+      ValueError: 
+
+  Returns:
+      HTMLNode: 
+  """
   if not block.startswith("```") or not block.endswith("```"):
     raise ValueError("invalid code block")
   text = block[4:-3]
@@ -70,18 +125,45 @@ def code_to_html(block):
   return ParentNode("pre", [code])
 
 def paragraph_to_html(block):
+  """Helper. Convert paragraph text (md) into html object structure. 
+
+  Args:
+      block (string): 
+
+  Returns:
+      ParentNode: 
+  """
   lines = block.splitlines()
   paragraph = " ".join(lines)
   children = text_to_children(paragraph)
   return ParentNode("p", children)
 
 def heading_to_html(block):
+  """Helper. Converts md heading into html nodes structure
+
+  Args:
+      block (string): 
+
+  Returns:
+      HTMLNode: 
+  """
   heading_hashes, content = block.split(maxsplit=1)    # ['###', 'content']  
   tag = heading_html(heading_hashes)    
   children = text_to_children(content)              
   return ParentNode(tag, children)
 
 def heading_html(heading_hashes):
+  """Helper for heading_to_html. Count number of # and returns appropriate tag <h1>, <h2> ...
+
+  Args:
+      heading_hashes (string): string of "###"
+
+  Raises:
+      ValueError: invalid num of #
+
+  Returns:
+      string: "h1", "h2", ...
+  """
   match len(heading_hashes):
     case 1:
       return "h1"
@@ -99,6 +181,14 @@ def heading_html(heading_hashes):
       raise ValueError("Invalid heading")
 
 def text_to_children(text):
+  """Helper. Takes text, and convert into html objects structure
+
+  Args:
+      text (string): md text
+
+  Returns:
+      HTMLNode: 
+  """
   text_nodes = text_to_textnodes(text) # list of TextNodes  
   children = []
   for node in text_nodes:    
